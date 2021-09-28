e6fc0e46cd55c9611034cf57e1ed590d6beca43a
Tomáš Holý <vantomas@gmail.com>
2021-09-28 13:52:23 +0200

    Fix  Return value of Phayes\GeoPHP\Geometry\Geometry::getProperty() must be an instance of Phayes\GeoPHP\Geometry\mixed, string returned

--- src/Geometry/Geometry.php
+++ src/Geometry/Geometry.php
@@ -354,7 +354,7 @@ abstract class Geometry
     $this->properties = $properties;
   }
 
-  public function getProperty($property): mixed {
+  public function getProperty($property) {
     return array_key_exists($property, $this->properties) ? $this->properties[$property] : NULL;
   }
 
